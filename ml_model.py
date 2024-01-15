3db2df1b3bd46bc44693e0cbb005810b3fda5d23
Madhumita <madhumita.kumari719@gmail.com>
2024-01-15 18:01:32 +0530

    added ML model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ML related code here")
+
+def model1(1):
+    print("adding code for ML model1")
+
+   def model1(1):
+    print("adding code for ML model2")
+
+    def model1(1):
+    print("adding code for ML model3") 
